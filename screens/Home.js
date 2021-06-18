cb17bf20db411952b33c004f0d91709c5fa92072
iamsid <developer.sid@outlook.com>
2021-06-18 14:11:24 +0300

    Screens-

---
+++ screens/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    View,
+    Text,
+
+} from "react-native"
+
+
+const Home = ({ navigation }) => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+        </View>
+    )
+
+    
+}
+
+export default Home;
---
+++ screens/Scan.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+    View,
+    Text,
+ 
+} from "react-native"
+
+
+
+const Scan = ({ navigation }) => {
+
+    function renderHeader() {
+        return (
+        <View>
+            <Text>yooh</Text>
+        </View>
+    )
+}}
+
+export default Scan
