2b0bf4960a77ee442b2e71a9c385af4b679b98a5
iamsid <developer.sid@outlook.com>
2021-06-18 14:09:15 +0300

    Setup

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//   root: true,
+//   extends: '@react-native-community',
+//   "parser": "babel-eslint"
+// };
+{
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+
+    "env": {
+        "es6": true
+    }
+}
---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
+
+import { SignUp } from "./screens"
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent"
+  }
+}
+const Stack = createStackNavigator()
+
+const App = () => {
+  return(
+    <NavigationContainer theme={theme}>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+            initialRouteName={'SignUp'}
+            >
+            <Stack.Screen name="SignUp" component={SignUp}/>
+        </Stack.Navigator>
+         
+    </NavigationContainer>
+
+  )
+}
+
+export default App;
